5ee417825dcd60c031e4ec54b2a606f88aee91eb
Oscar Krause <oscar.krause@collinwebdesigns.de>
2022-12-19 22:16:42 +0100

    main.py - leases - rollback to @0df63016

--- app/main.py
+++ app/main.py
@@ -234,10 +234,9 @@ async def lease(request: Request):
     cur_time = datetime.utcnow()
     # venv/lib/python3.9/site-packages/nls_dal_service_instance_dls/schema/service_instance/V1_0_21__product_mapping.sql
     response = {
+        # GRID-Virtual-WS 2.0 CONCURRENT_COUNTED_SINGLE
         "active_lease_list": [
-            "BE276D7B-2CDB-11EC-9838-061A22468B59",  # 'BE276D7B-2CDB-11EC-9838-061A22468B59','BDFBE16D-2CDB-11EC-9838-061A22468B59' => 'NVIDIA Virtual PC','NVIDIA Virtual PC'
-            "BE276EFE-2CDB-11EC-9838-061A22468B59",  # 'BE276EFE-2CDB-11EC-9838-061A22468B59','BDFBE308-2CDB-11EC-9838-061A22468B59' => 'NVIDIA RTX Virtual Workstation','NVIDIA RTX Virtual Workstation'
-            "BE276FF0-2CDB-11EC-9838-061A22468B59",  # 'BE276FF0-2CDB-11EC-9838-061A22468B59','BDFBE405-2CDB-11EC-9838-061A22468B59' => 'NVIDIA vGaming','NVIDIA vGaming'
+            "BE276D7B-2CDB-11EC-9838-061A22468B59"
         ],
         "sync_timestamp": cur_time,
         "prompts": None

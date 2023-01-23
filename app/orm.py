f7ef8d76b6d2f07b536761a046b6db78483a669c
Oscar Krause <oscar.krause@collinwebdesigns.de>
2023-01-23 07:12:02 +0100

    fixed Origin.delete()

--- app/orm.py
+++ app/orm.py
@@ -57,12 +57,12 @@ class Origin(Base):
         session.close()
 
     @staticmethod
-    def delete(engine: Engine, origins: ["Origin"] = None) -> int:
+    def delete(engine: Engine, origin_refs: [str] = None) -> int:
         session = sessionmaker(bind=engine)()
-        if origins is None:
+        if origin_refs is None:
             deletions = session.query(Origin).delete()
         else:
-            deletions = session.query(Origin).filter(Origin.origin_ref in origins).delete()
+            deletions = session.query(Origin).filter(Origin.origin_ref in origin_refs).delete()
         session.commit()
         session.close()
         return deletions
